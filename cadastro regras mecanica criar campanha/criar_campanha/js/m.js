0d3c2d645d8248a9f13aa56484c232d0a5f586ab
DaviTodt <davi.todt@souunit.com.br>
2024-10-21 19:29:15 -0300

    Add files via upload

---
+++ cadastro regras mecanica criar campanha/criar_campanha/js/m.js
@@ -0,0 +1,6 @@
+let b=document.getElementById("enviar");
+
+b.addEventListener("click",function(){
+    sessionStorage.setItem("addc",true);
+    window.location.href = "../tipos de campanhas/Tipos_de_campanha.html";
+});
