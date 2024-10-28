899e8c7e26216df71957c5eee298471ce98dfe79
DaviTodt <davi.todt@souunit.com.br>
2024-10-28 20:25:25 -0300

    Add files via upload

--- tipos de campanhas/ldc.js
+++ tipos de campanhas/ldc.js
@@ -1,23 +1,26 @@
-const type = sessionStorage.getItem("type");
-const addc = sessionStorage.getItem("addc");
-const edc = sessionStorage.getItem("edc");
+let type = sessionStorage.getItem("type");
+let addc = sessionStorage.getItem("addc");
+let edc = sessionStorage.getItem("edc");
+let ccamp = sessionStorage.getItem("changecamp");
 let camp_div=document.getElementById("camp_div")
 let item_i=3;
-/*
+
 if(type == "adm")
 {
     for(var i=0;i<4;i++)
     {
-        var cs=document.getElementById("camp" + i)
-        cs.innerHTML += `<p class=editar onclick="editc("camp" + ${i})">Editar</p>`;
+        var cs=document.getElementById("camp" + i);
+        cs.innerHTML += `<a class="pc" onclick="editc('${"camp" + i}')">Editar</a>`;
     }
 }
 
 function editc(item)
 {
-    var curcamp=document.getElementById(item);
+    sessionStorage.setItem("edc",true);
+    sessionStorage.setItem("campid",item);
+    window.location.href = "../cadastro regras mecanica criar campanha/criar_campanha/criar_campanha.html";
 }
-*/
+
 function addcamp()
 {
     item_i++;
@@ -25,17 +28,24 @@ function addcamp()
     var camp_name=sessionStorage.getItem("name_camp");
     var camp_desc=sessionStorage.getItem("desc_name");
 
-    var camp = ` <div id="camp_div">
+    var camp = ` <div id="${'camp' + item_i}">
     <h2 class="data">${camp_name} Active</h2>
-    <div class="campanha active" id="camp" + ${item_i}>
+    <div class="campanha active">
     <p class="desc-p-2">Insira uma descrição e um tipo de benefício</p>
     <p class="dataa">Data:XX/XX/2024</p>
         <div class="desc">
         <p>Descrição: ${camp_desc}</p>
         </div>
+    </div>
     </div>`;
 
     camp_div.innerHTML += camp;
+
+    if(type == "adm")
+    {
+        var curcamp=document.getElementById("camp" + item_i);
+        curcamp.innerHTML += `<a class="pc" onclick="editc(${'camp' + item_i})">Editar</a>`;
+    }
 }
 
 if(addc == "true")
@@ -49,4 +59,31 @@ if(edc == "true")
     sessionStorage.setItem("name_camp","Natal");
     sessionStorage.setItem("desc_name","Campanha de fim de ano, focada em promoções de Natal, decorações e presentes.");
     window.location.href = "../cadastro regras mecanica criar campanha/criar_campanha/criar_campanha.html";
+}
+
+if(ccamp == "true")
+{
+    var curcamp=sessionStorage.getItem("campid");
+    var edcamp=camp_div.querySelector("#" + curcamp);
+    var camp_name=sessionStorage.getItem("name_camp");
+    var camp_desc=sessionStorage.getItem("desc_name");
+
+    var camp = `
+    <h2 class="data">${camp_name} Active</h2>
+    <div class="campanha active">
+    <p class="desc-p-2">Insira uma descrição e um tipo de benefício</p>
+    <p class="dataa">Data:XX/XX/2024</p>
+        <div class="desc">
+        <p>Descrição: ${camp_desc}</p>
+        </div>
+    </div>`;
+
+    edcamp.innerHTML = camp;
+
+    if(type == "adm")
+    {
+        edcamp.innerHTML += `<a class="pc" onclick="editc('${curcamp}')">Editar</a>`;
+    }
+
+    sessionStorage.setItem("changecamp",false);
 }
